528dd5ed5452dd0c74c5a117c3050a8775ccecd5
wangenzhao <wangenzhao2016@gmail.com>
2017-03-10 20:33:05 +0800

    add project

---
+++ app/src/main/java/com/wangez/support/demo/MainActivity.java
@@ -0,0 +1,19 @@
+package com.wangez.support.demo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * @author 王恩钊
+ * @time 2017/3/10 20:06
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
